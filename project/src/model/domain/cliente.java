aab921a899473c808f415abda465caa644db4f74
Matheus Claudino <matheusclaudin@gmail.com>
2015-03-24 11:24:56 -0300

    notação hibernate

--- project/src/model/domain/cliente.java
+++ project/src/model/domain/cliente.java
@@ -1,9 +1,11 @@
 package model.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class cliente {
 	
 	@Id
