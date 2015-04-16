8d906fd0763077877a2a8e104e20d2048b192bd4
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-16 19:55:28 -0300

    Classe de dominio Titulo criada.

---
+++ project/src/model/domain/Titulo.java
@@ -0,0 +1,82 @@
+package model.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Titulo {
+	@Id
+    @GeneratedValue
+	private long id;
+	@Column(nullable = false)
+	private String nome;
+	private String ano;
+	private String sinopse;
+	private String categoria;
+	@ManyToMany(cascade = CascadeType.ALL)
+	private Set<Ator> atores = new HashSet<Ator>();
+	@ManyToOne
+	private Diretor diretor;
+	
+	public Titulo(){
+				
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Diretor getDiretor() {
+		return diretor;
+	}
+
+	public void setDiretor(Diretor diretor) {
+		this.diretor = diretor;
+	}
+		
+}
+
