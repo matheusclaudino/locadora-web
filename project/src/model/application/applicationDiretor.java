c9ed0ecc5b664e37dcbd7495adc9a5b00a8d5f42
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-16 20:49:26 -0300

    Método para cadastrar diretor implementado.

--- project/src/model/application/applicationDiretor.java
+++ project/src/model/application/applicationDiretor.java
@@ -1,5 +1,11 @@
 package model.application;
 
+import model.domain.Diretor;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
 public class applicationDiretor {
 	
 	public static final int INSCREVER_NOVO_DIRETOR_OK = 1 ;
@@ -9,6 +15,15 @@ public class applicationDiretor {
 		if(nome.equals(""))
 			return INSCREVER_NOVO_DIRETOR_ERRO;
 		
+		Diretor d = new Diretor(nome);
+		
+		SessionFactory sessions = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessions.openSession();
+		
+		session.beginTransaction();
+		session.save(d);
+		session.getTransaction().commit();
+		session.close();
 		
 		return INSCREVER_NOVO_DIRETOR_OK;
 	}
