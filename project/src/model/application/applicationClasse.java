b836af52fe937d46b9ed35654e499256d517a915
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-16 21:28:09 -0300

    Método para inscrever nova classe implementado.

--- project/src/model/application/applicationClasse.java
+++ project/src/model/application/applicationClasse.java
@@ -1,5 +1,30 @@
 package model.application;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import model.domain.Classe;
+
 public class applicationClasse {
+
+	private static final int INSCREVER_NOVA_CLASSE_OK = 1;
+	private static final int INSCREVER_NOVA_CLASSE_ERRO = 0;
 	
+	public static int inscreverNovaClasse(String nome, double valor, int prazo){
+		if(nome.equals(""))
+			return INSCREVER_NOVA_CLASSE_ERRO;
+		
+		Classe c = new Classe(nome, valor, prazo);
+		
+		SessionFactory sessions = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessions.openSession();
+		
+		session.beginTransaction();
+		session.save(c);
+		session.getTransaction().commit();
+		session.close();
+		
+		return INSCREVER_NOVA_CLASSE_OK;
+	}
 }
