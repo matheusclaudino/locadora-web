84d9e093a45b6e254ff812b71364d8f0647a22ae
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-17 01:11:39 -0300

    Adicionado novos atributos e seus respectivos getters and setters.

--- project/src/controller/ctrlCadastrarTitulo.java
+++ project/src/controller/ctrlCadastrarTitulo.java
@@ -3,7 +3,6 @@ package controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.domain.Ator;
-import model.domain.Socio;
+import model.domain.Classe;
+import model.domain.Diretor;
+import model.domain.Distribuidor;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -63,18 +64,37 @@ public class ctrlCadastrarTitulo extends HttpServlet {
 			
 			SessionFactory  sessions = new AnnotationConfiguration().configure().buildSessionFactory();
 			//Pegando todos os atores selecionados no checkbox no banco
-			for(String s: atores){
-				Session session = sessions.openSession();
-				int id = Integer.parseInt(s.toString());
-				
-				String strQuery = "FROM Ator WHERE id = " + id;
-				session.beginTransaction();
-				Query qr = session.createQuery(strQuery);
-				Ator a = (Ator)qr.uniqueResult();
-				
-				ator.add(a);
-				session.close();
-			}
+				for(String s: atores){
+					Session session = sessions.openSession();
+					int id = Integer.parseInt(s.toString());
+					
+					String strQuery = "FROM Ator WHERE id = " + id;
+					session.beginTransaction();
+					Query qr = session.createQuery(strQuery);
+					Ator a = (Ator)qr.uniqueResult();
+					
+					ator.add(a);
+					session.close();
+				}
+			
+			SessionFactory s = new AnnotationConfiguration().configure().buildSessionFactory();
+			Session session = s.openSession();
+			
+			String queryClasse = "from Classe WHERE id =" + classe;
+			String queryDiretor = "FROM Diretor WHERE id = " +  diretor;
+			String queryDistribuidor = "from Distribuidor WHERE cnpj = " + distribuidor;
+			
+			session.beginTransaction();
+			Query qryClasse = session.createQuery(queryClasse);
+			Query qryDiretor = session.createQuery(queryDiretor);
+			Query qryDistribuidor = session.createQuery(queryDistribuidor);
+
+			Classe cla =  (Classe) qryClasse.uniqueResult();
+			Diretor dir = (Diretor) qryDiretor.uniqueResult();
+			Distribuidor dis = (Distribuidor) qryDistribuidor.uniqueResult();
+			
+			session.close();
+			
 			
 			
 		}else{
