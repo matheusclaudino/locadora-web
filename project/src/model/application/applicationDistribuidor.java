75f21c718370f91537930552af951edccb8bb2fc
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-16 22:05:29 -0300

    Método para cadastrar distribuidor implementado e criado atributos para tratamento de erro

--- project/src/model/application/applicationDistribuidor.java
+++ project/src/model/application/applicationDistribuidor.java
@@ -1,5 +1,30 @@
 package model.application;
 
-public class applicationDistribuidor {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 
+import model.domain.Distribuidor;
+
+public class applicationDistribuidor {
+	
+	public static final int INSCREVER_NOVO_DISTRIBUIDOR_OK = 1;
+	public static final int INSCREVER_NOVO_DISTRIBUIDOR_ERRO = 0;
+	
+	public static int inscreverNovoDistribuidor(int cnpj, String razao){
+		if(razao.equals(""))
+			return INSCREVER_NOVO_DISTRIBUIDOR_ERRO;
+		
+		Distribuidor d = new Distribuidor(cnpj, razao);
+		
+		SessionFactory sessions = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessions.openSession();
+		
+		session.beginTransaction();
+		session.save(d);
+		session.getTransaction().commit();
+		session.close();
+		
+		return INSCREVER_NOVO_DISTRIBUIDOR_OK;
+	}
 }
