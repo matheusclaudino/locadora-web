107e39c8407afad7b742c68e2c240cedbac89100
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-16 23:21:10 -0300

    get e set do atributo id adicionado.

--- project/src/model/domain/Classe.java
+++ project/src/model/domain/Classe.java
@@ -26,6 +26,15 @@ public class Classe {
 		this.prazoDevolucao = prazoDevolucao;
 	}
 	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
 	}
