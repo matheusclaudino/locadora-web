4546a08803e3c02bb075e68ef72fa9158bd35606
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-17 02:08:01 -0300

    Método de inserção de categoria implementado.

--- project/src/model/application/applicationCategoria.java
+++ project/src/model/application/applicationCategoria.java
@@ -1,5 +1,29 @@
 package model.application;
 
-public class applicationCategoria {
+import model.domain.Categoria;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class applicationCategoria {
+	public static final int INSCREVER_NOVA_CATEGORIA_OK = 1 ;
+	public static final int INSCREVER_NOVA_CATEGORIA_ERRO = 0;
+	
+	public static int inscreverNovaCategoria(String nome){
+		if(nome.equals(""))
+			return INSCREVER_NOVA_CATEGORIA_ERRO;
+		
+		Categoria c = new Categoria(nome);
+		
+		SessionFactory sessions = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessions.openSession();
+		
+		session.beginTransaction();
+		session.save(c);
+		session.getTransaction().commit();
+		session.close();
+		
+		return INSCREVER_NOVA_CATEGORIA_OK;
+	}
 }
