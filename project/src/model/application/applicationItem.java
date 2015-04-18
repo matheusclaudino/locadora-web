d987700b735327ccedfb4bef16a471632edff64e
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-18 18:02:49 -0300

    Método para cadastrar novo item implementado.

--- project/src/model/application/applicationItem.java
+++ project/src/model/application/applicationItem.java
@@ -1,5 +1,31 @@
 package model.application;
 
-public class applicationItem {
+import model.domain.Item;
+import model.domain.TipoItem;
+import model.domain.Titulo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class applicationItem {
+	public static final int INSCREVER_NOVO_ITEM_OK = 1;
+	public static final int INSCREVER_NOVO_ITEM_ERRO = 0;
+	
+	public static int inscreverNovoItem(long numeroSerie, String data, Titulo titulo, TipoItem tipoItem){
+		if(numeroSerie == 0)
+			return INSCREVER_NOVO_ITEM_ERRO;
+		
+		Item i = new Item(numeroSerie, data, titulo, tipoItem);
+		
+		SessionFactory sessions = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session s = sessions.openSession();
+		
+		s.beginTransaction();
+		s.save(i);
+		s.getTransaction().commit();
+		s.close();
+		
+		return INSCREVER_NOVO_ITEM_OK;
+	}
 }
