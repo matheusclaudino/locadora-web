6b6f02e3e30677d13579563dc5fedb28e2ef5e6e
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-18 23:58:54 -0300

    Mapeando o relacionamento com Locacao, e criando seus respectivos métodos.

--- project/src/model/domain/Cliente.java
+++ project/src/model/domain/Cliente.java
@@ -1,9 +1,15 @@
 package model.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Cliente {
@@ -16,6 +22,9 @@ public class Cliente {
 	private String data_nascimento;
 	private char sexo;
 	private Boolean ativo;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private Set<Locacao> locacoes;
+	
 	
 	public Cliente() {
 		super();
@@ -30,42 +39,60 @@ public class Cliente {
 		this.data_nascimento = data_nascimento;
 		this.sexo = sexo;
 		this.ativo = ativo;
+		this.locacoes = new HashSet<Locacao>();
 	}
 
+	public void inserirLocacao(Locacao l){
+		locacoes.add(l);
+	}
+		
+	public Set<Locacao> getLocacoes() {
+		return locacoes;
+	}
 
+	public void setLocacoes(Set<Locacao> locacoes) {
+		this.locacoes = locacoes;
+	}
 
 	public int getNumero_inscricao() {
 		return numero_inscricao;
 	}
+	
 	public void setNumero_inscricao(int numero_inscricao) {
 		this.numero_inscricao = numero_inscricao;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getData_nascimento() {
 		return data_nascimento;
 	}
+	
 	public void setData_nascimento(String data_nascimento) {
 		this.data_nascimento = data_nascimento;
 	}
+	
 	public char getSexo() {
 		return sexo;
 	}
+	
 	public void setSexo(char sexo) {
 		this.sexo = sexo;
 	}
+	
 	public Boolean getAtivo() {
 		return ativo;
 	}
+	
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
 	}
-	
-	
 	
 
 }
