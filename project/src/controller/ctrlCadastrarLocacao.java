8405e872d8fac4e27c40554dce8662727afe8445
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 15:00:40 -0300

    consultas de titulo adicionada e condicional para pegar o item criada.

--- project/src/controller/ctrlCadastrarLocacao.java
+++ project/src/controller/ctrlCadastrarLocacao.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.application.applicationLocacao;
 import model.domain.Cliente;
+import model.domain.Item;
+import model.domain.Titulo;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -58,17 +61,33 @@ public class ctrlCadastrarLocacao extends HttpServlet {
 			Session session = s.openSession();
 			
 			String queryCliente = "FROM Cliente WHERE numero_inscricao =" + numero;
-
+			/*String query = "select item.id, titulo.nome, tipoitem.nome from item, titulo, tipoitem WHERE titulo.id = item.titulo_id";*/
+			String queryTitulo = "FROM Titulo WHERE id =" + titulo;
+			String queryItem = "FROM Item";
+			
 			
 			session.beginTransaction();
 			Query qryCliente = session.createQuery(queryCliente);
-
+			Query qryTitulo = session.createQuery(queryTitulo);
+			Query qryItem = session.createQuery(queryItem);
+			
 			Cliente cliente =  (Cliente) qryCliente.uniqueResult();
-
+			Titulo t = (Titulo) qryTitulo.uniqueResult();
+			List<Item> itens = qryItem.list();
+			Item itemEscolhido = new Item();
+			
+			//pegando o item a ser locado
+			for(Item item: itens){
+				if((t.getId() == item.getTitulo().getId()) && (Integer.parseInt(tipo) == item.getTipoItem().getId())){
+					itemEscolhido = item;
+				}
+			}
+			
+			System.out.println("ITEM: " + itemEscolhido.getTipoItem().getNome());
 			
 			session.close();
 			
-			if(applicationLocacao.inscreverNovaLocacao(dataLocacao, dataPrevista, valor, cliente, item) == applicationLocacao.INSCREVER_NOVA_LOCACAO_OK){
+			if(applicationLocacao.inscreverNovaLocacao(dataLocacao, dataPrevista, Double.parseDouble(valor), cliente, itemEscolhido) == applicationLocacao.INSCREVER_NOVA_LOCACAO_OK){
 				
 			}else{
 				
