83e8fa24d08674eadb773dfb4683d0515601e93f
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 15:02:25 -0300

    Método para cadastrar locacao implementado

--- project/src/model/application/applicationLocacao.java
+++ project/src/model/application/applicationLocacao.java
@@ -2,6 +2,11 @@ package model.application;
 
 import model.domain.Cliente;
 import model.domain.Item;
+import model.domain.Locacao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 
 public class applicationLocacao {
 	public static final int INSCREVER_NOVA_LOCACAO_OK = 1;
@@ -9,6 +14,16 @@ public class applicationLocacao {
 	
 	public static int inscreverNovaLocacao(String dataLocacao, String dataDevolucaoPrevista, double valorCobrado,
 			Cliente cliente, Item item){
+		
+		Locacao l = new Locacao(dataLocacao, dataDevolucaoPrevista, valorCobrado, cliente, item);
+		
+		SessionFactory sessions = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session s = sessions.openSession();
+		
+		s.beginTransaction();
+		s.save(l);
+		s.getTransaction().commit();
+		s.close();
 		
 		return INSCREVER_NOVA_LOCACAO_OK;
 	}
