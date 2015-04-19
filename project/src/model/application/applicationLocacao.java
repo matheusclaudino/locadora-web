96c4db5bc05b0cf89d32394e17f93e54e997c19c
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 12:42:51 -0300

    Encapsulamento dos atributos modificados, e método para cadastrar locacao criado.

--- project/src/model/application/applicationLocacao.java
+++ project/src/model/application/applicationLocacao.java
@@ -1,7 +1,15 @@
 package model.application;
 
-public class applicationLocacao {
-	private static final int INSCREVER_NOVA_LOCACAO_OK = 1;
-	private static final int INSCREVER_NOVA_LOCACAO_ERRO = 0;
+import model.domain.Cliente;
+import model.domain.Item;
 
+public class applicationLocacao {
+	public static final int INSCREVER_NOVA_LOCACAO_OK = 1;
+	public static final int INSCREVER_NOVA_LOCACAO_ERRO = 0;
+	
+	public static int inscreverNovaLocacao(String dataLocacao, String dataDevolucaoPrevista, double valorCobrado,
+			Cliente cliente, Item item){
+		
+		return INSCREVER_NOVA_LOCACAO_OK;
+	}
 }
