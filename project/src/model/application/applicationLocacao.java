8853e7d1ecd4923c832dcfe0349055f0fcb15911
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 12:30:45 -0300

    Application de Locacao criada com os atributos para erro.

---
+++ project/src/model/application/applicationLocacao.java
@@ -0,0 +1,7 @@
+package model.application;
+
+public class applicationLocacao {
+	private static final int INSCREVER_NOVA_LOCACAO_OK = 1;
+	private static final int INSCREVER_NOVA_LOCACAO_ERRO = 0;
+
+}
