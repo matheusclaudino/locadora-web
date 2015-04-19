7cd4d0e906018771e9d2dcdf5e9f0860cc51c6c9
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 11:06:15 -0300

    construtor criado e os getters e setters dos atributos.

--- project/src/model/domain/Pagamento.java
+++ project/src/model/domain/Pagamento.java
@@ -19,6 +19,51 @@ public class Pagamento {
 	
 	@ManyToMany(cascade = CascadeType.ALL)
 	private Set<Locacao> locacoes = new HashSet<Locacao>();
+
+	public Pagamento() {
+		super();
+	}
+
+	public Pagamento(String data, double valor) {
+		super();
+		this.data = data;
+		this.valor = valor;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Set<Locacao> getLocacoes() {
+		return locacoes;
+	}
+
+	public void setLocacoes(Set<Locacao> locacoes) {
+		this.locacoes = locacoes;
+	}
 	
+	public void inserirLocacao(Locacao l){
+		locacoes.add(l);
+	}
 	
 }
