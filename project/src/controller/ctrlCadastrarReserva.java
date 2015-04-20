5c29f00c22ded0d764054fb5d63e2b6084ef9c2a
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 21:44:46 -0300

    Chamada do método da application para cadastrar a classe implementada.

--- project/src/controller/ctrlCadastrarReserva.java
+++ project/src/controller/ctrlCadastrarReserva.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.application.applicationReserva;
+import model.domain.Cliente;
+import model.domain.TipoItem;
+import model.domain.Titulo;
 
 /**
  * Servlet implementation class ctrlCadastrarReserva
@@ -41,6 +47,8 @@ public class ctrlCadastrarReserva extends HttpServlet {
 			String cliente = request.getParameter("cliente");
 			String titulo = request.getParameter("titulo");
 			String tipoItem = request.getParameter("tipo");
+			
+			applicationReserva.inscreverNovaReserva(cliente, titulo, tipoItem);
 			
 		}
 	}
