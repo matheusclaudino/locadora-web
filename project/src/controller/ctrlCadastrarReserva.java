1d759b0ce9703a3dadd73615879c45f065ba90eb
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 21:11:43 -0300

    Criado o controlador de Reserva e implementado a funcionalidade para pegar as informações da requisição.

---
+++ project/src/controller/ctrlCadastrarReserva.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ctrlCadastrarReserva
+ */
+@WebServlet("/ctrlCadastrarReserva")
+public class ctrlCadastrarReserva extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ctrlCadastrarReserva() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String operacao = request.getParameter("operacao");
+		
+		if(operacao.equals("inscreverNovaReserva")){
+			
+			String cliente = request.getParameter("cliente");
+			String titulo = request.getParameter("titulo");
+			String tipoItem = request.getParameter("tipo");
+			
+		}
+	}
+
+}
