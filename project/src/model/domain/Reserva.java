db5beb769b9772adbb2e24c8088d033537509700
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-19 21:03:34 -0300

    Getters e Setters adicionado.

--- project/src/model/domain/Reserva.java
+++ project/src/model/domain/Reserva.java
@@ -22,4 +22,57 @@ public class Reserva {
 	private TipoItem tipoItem;
 	
 	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public String getHora() {
+		return hora;
+	}
+	
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public Titulo getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(Titulo titulo) {
+		this.titulo = titulo;
+	}
+	
+	public TipoItem getTipoItem() {
+		return tipoItem;
+	}
+	
+	public void setTipoItem(TipoItem tipoItem) {
+		this.tipoItem = tipoItem;
+	}
+	
+	
+	
+	
+	
+	
 }
