8bbc617420b28e4cc292b783c8a794d8b1711090
Matheus Claudino <matheusclaudin@gmail.com>
2015-04-23 08:48:41 -0300

    Criado método para inscrever o pagamento, e adicionado a funcionalidade para pegar a data do sistema;

--- project/src/model/application/applicationPagamento.java
+++ project/src/model/application/applicationPagamento.java
@@ -1,5 +1,19 @@
 package model.application;
 
+import java.util.GregorianCalendar;
+
 public class applicationPagamento {
 
+	public static void inscreverNovaLocacao(){
+		GregorianCalendar calendar = new GregorianCalendar(); 
+		int hora = calendar.get(calendar.HOUR_OF_DAY);
+		int day = calendar.get(calendar.DATE);
+		int month = calendar.get(calendar.MONTH);
+		int year = calendar.get(calendar.YEAR);
+		
+		String data = String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.valueOf(day) ;
+		
+		
+	}
+	
 }
